e222266f86bb99a15e0d3db46c91e01dc1a472f1
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-25 16:24:33 +0200

    Update test to v4.0

--- tests/test_sale_opportunity.py
+++ tests/test_sale_opportunity.py
@@ -3,22 +3,12 @@
 # this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleOpportunityTestCase(unittest.TestCase):
+class SaleOpportunityTestCase(ModuleTestCase):
     'Test SaleOpportunity module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_opportunity')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_opportunity')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_opportunity'
 
 
 def suite():
